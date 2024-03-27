47f93cbf28df4d49211400e995d86a752058a2fb
Matias (Wazted) <matias.campos@epitech.eu>
2024-03-27 23:38:45 +0100

    feat(add): scroll snap like youtube shorts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+'use client'
+import { RefObject, useRef, useState } from 'react'
+
 export default function App() {
+  const [refs] = useState<RefObject<HTMLDivElement>[]>(Array.from({ length: 4 }).map(() => useRef(null)))
+  const slides = [
+    <div className="h-[calc(100vh-40px)] w-full bg-blue-200">ababab</div>,
+    <div className="h-[calc(100vh-40px)] w-full bg-red-200">bxaxax</div>,
+    <div className="h-[calc(100vh-40px)] w-full bg-green-200">csdasd</div>,
+    <div className="h-[calc(100vh-40px)] w-full bg-yellow-200">deerererer</div>
+  ]
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      Pres page
+    <main className="h-screen flex flex-col items-center justify-between snap-y snap-mandatory scroll-pt-10 overflow-auto">
+      {slides.map((slide, index) => (
+        <div className="w-full snap-center" ref={refs[index]} key={'pres-slide-' + index}>
+          {slides[index]}
+        </div>
+      ))}
     </main>
-  );
+  )
 }
